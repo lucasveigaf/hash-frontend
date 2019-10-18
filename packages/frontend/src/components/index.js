5ce6f1e590eb0076918fa2d284ce85d05a1cca4c
lucas veiga fujicava <lucasveigaf@gmail.com>
2019-10-17 23:06:48 -0300

    Register the Anticipation html tag

--- packages/frontend/src/components/index.js
+++ packages/frontend/src/components/index.js
@@ -1,9 +1,11 @@
 import { registerComponents } from '@hash-mono/stylesystem';
 
+import Anticipation from './Anticipation';
 import AnticipationLabel from './AnticipationLabel';
 
 function register() {
   registerComponents();
+  customElements.define('h-anticipation', Anticipation);
   customElements.define('h-anticipation-label', AnticipationLabel);
 }
 
