a3584b5745573e75c2a59517023ce56ae2d64b03
lucas veiga fujicava <lucasveigaf@gmail.com>
2019-10-17 22:52:27 -0300

    Add stylesystem theme constants

---
+++ packages/stylesystem/src/theme.js
@@ -0,0 +1,9 @@
+export default {
+  fontSizes: ['11px', '14px', '16px', '20px', '24px'],
+  colors: {
+    grey: ['#656565', '#CECECE', 'rgba(209, 220, 227, 0.18)', '#CECECE', '#F2F2F2', '#DDE6E9'],
+    blue: ['#3D75BB', '#5D9CEC', '#66AFE9'],
+  },
+  borderRadius: '4px',
+  fontFamily: 'Source Sans Pro',
+};
