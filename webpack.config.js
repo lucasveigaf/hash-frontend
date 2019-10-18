c16f6d716af986a20609914ecf51f1df4bccf6d7
lucas veiga fujicava <lucasveigaf@gmail.com>
2019-10-17 22:47:17 -0300

    Configure webcomponentsjs polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -21,7 +22,14 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: 'packages/frontend/src/index.html',
       filename: 'index.html',
-    })
+    }),
+    new CopyWebpackPlugin([
+      {
+        context: './node_modules/@webcomponents/webcomponentsjs',
+        from: '**/*.js',
+        to: 'webcomponents',
+      },
+    ]),
   ],
   devtool: 'source-map',
   optimization: {
